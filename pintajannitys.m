e0687803c1e0cb3308ec60c0ea3fd65a153e6f69
Helmer Piirilä <helmer.piirila@student.tut.fi>
2017-12-21 11:37:15 +0200

    More development

--- pintajannitys.m
+++ pintajannitys.m
@@ -7,8 +7,7 @@ B = 235.8*1e-3;
 b = -0.625;
 mu = 1.256;
 
-tau = 1 - T/Tc;
-jannitys = B * tau^mu * (1 - b*tau);
-
+tau = 1 - T./Tc;
+jannitys = B * tau.^mu .* (1 - b*tau);
 
 end
--- supersaturaatio.m
+++ supersaturaatio.m
@@ -1,14 +1,11 @@
-function [SR,ps] = supersaturaatio(dp,T)
+function SR = supersaturaatio(dp,T)
 
 M = (2 * 1.008 + 15.999)*1e-3;  % veden moolimassa, kg/mol
-gamma = pintajannitys(T);       % veden pintajännitys, N/m
+sigma = pintajannitys(T);       % veden pintajännitys, N/m
 rho = 1000;                     % veden tiheys, kg/m^3
 R = 8.3144598;                  % kaasuvakio, J/mol*K
 
-% lasketaan saturaatiohöyrynpaine ps
-ps = tasapainoPsw(T);
-
 % lasketaan supersaturaatiosuhde
-SR = exp(4*M*gamma ./ (rho*R*T*dp*1e-9));
+SR = exp(4*M*sigma ./ (rho*R*T.*dp));
 
 end
